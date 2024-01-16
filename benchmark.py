b092330b422875f7b5d4b3239e11ca3904551ff8
z-jiaming <916678091@qq.com>
2024-01-16 15:24:57 +0800

    better reading.

--- benchmark.py
+++ benchmark.py
@@ -24,13 +24,10 @@ parser.add_argument('--overwrite',
                     action='store_true')
 
 # https://github.com/davisvideochallenge/davis2017-evaluation/blob/d34fdef71ce3cb24c1a167d860b707e575b3034c/davis2017/evaluation.py#L85
-parser.add_argument(
-    '-d',
-    '--do_not_skip_first_and_last_frame',
-    help=
-    'By default, we skip the first and the last frame in evaluation following DAVIS semi-supervised evaluation.'
-    'They should not be skipped in unsupervised evaluation.',
-    action='store_true')
+parser.add_argument('-d', '--do_not_skip_first_and_last_frame',
+                    help='By default, we skip the first and the last frame in evaluation following DAVIS semi-supervised evaluation. They should not be skipped in unsupervised evaluation.',
+                    action='store_true')
+
 args = parser.parse_args()
 
 benchmark(
